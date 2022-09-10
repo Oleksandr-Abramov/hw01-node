f69345f8af9168b8c77a4c066a8bff45a5da92ab
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-09-11 00:53:10 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hw01-node
+
+https://ibb.co/jLKGHJY
+https://ibb.co/pPKWqGt
+https://ibb.co/DQZWmmz
+https://ibb.co/NnScK6C
---
+++ contacts.js
@@ -0,0 +1,53 @@
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const updateContacts = (contacts) => {
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
+
+const idMaker = async () => {
+  const contacts = await listContacts();
+  const sortedContactsId = contacts.map((el) => Number(el.id)).sort((a, b) => a - b);
+  let checkId = false;
+  const contactsId = sortedContactsId.reduce((acc, el, idx, arr) => {
+    if (!checkId && arr[idx + 1] - el > 1) {
+      acc = el + 1;
+      checkId = true;
+    }
+    return acc;
+  }, sortedContactsId[sortedContactsId.length - 1] + 1);
+  return String(contactsId);
+};
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((el) => el.id === String(contactId));
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((el) => el.id === String(contactId));
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: await idMaker(), name, email, phone };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
